d11fa97e2fa3c5b0d5be008a1febf2d171edd63c
F_k <goisu0880@gmail.com>
2022-12-04 15:52:41 +0900

    new file:   Controller/main_view_start.py 	modified:   main.py

---
+++ Controller/main_view_start.py
@@ -0,0 +1,5 @@
+# 自作モジュール
+from View import MainScreenView
+
+# メイン画面起動
+MainScreenView
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # 自作モジュール
-from View import MainScreenView
+from Controller import main_view_start
 from Model import data_save
 
 # フォルダ作成
 data_save.create_folder_f()
 
 # メイン画面起動
-MainScreenView
+main_view_start
 
 
